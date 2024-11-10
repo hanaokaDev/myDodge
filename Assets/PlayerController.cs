ba249d64c4a23f0c4c719edaa558a5886d90b468
User <user@DESKTOP-EDS44JG>
2024-11-10 14:51:57 +0900

    First Commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController
+{
+    public Rigidbody playerRigidBody;
+    public float speed = 8f;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}

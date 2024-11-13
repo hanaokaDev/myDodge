0cd5fcd4534c4e132c7ea74268a4bcedcfeb00f0
User <user@DESKTOP-EDS44JG>
2024-11-13 23:49:28 +0900

    Bullet.cs

---
+++ Assets/Bullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    private Rigidbody bulletRigidBody;
+    void Start()
+    {
+        bulletRigidBody = GetComponent<Rigidbody>();
+        bulletRigidBody.linearVelocity = transform.forward * speed;
+        Destroy(gameObject, 3f); // 3ÃÊµÚ ÆÄ±«
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerController playerController = other.GetComponent<PlayerController>();
+        if (playerController != null)
+        {
+            playerController.Die();
+        }
+        // if (other.CompareTag("Player"))
+        // {
+        //     other.GetComponent<PlayerController>().Die();
+        // }
+        // Destroy(gameObject);
+    }
+
+}
